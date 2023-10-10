63481befd5c2c6d6b5fb8dafce35b7ab68cc0b3c
prashantpaddune <padadunep@gmail.com>
2023-10-10 17:28:02 +0530

    repocard: show Avatar, Repo name, Stars, Description, language
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- src/components/RepoCard/index.js
+++ src/components/RepoCard/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { RepoCardStyled } from './styles';
+import {RepoCardStyled, Avatar, RepoName, RepoDetails, Description, GitHubLink} from './styles';
 
 const RepoCard = ({ repo = {} }) => {
     return (
         <RepoCardStyled>
-            <h2>{repo.name}</h2>
-            <p>{repo.description}</p>
-            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            <Avatar src={repo.owner.avatar_url} alt={`${repo.owner.login}'s avatar`} />
+            <RepoName>{repo.name}</RepoName>
+            <RepoDetails>
+                <div><strong>Stars:</strong> {repo.stargazers_count}</div>
+                <div><strong>Language:</strong> {repo.language || 'N/A'}</div>
+            </RepoDetails>
+            <Description>{repo.description}</Description>
+            <GitHubLink href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</GitHubLink>
         </RepoCardStyled>
     );
 }
--- src/components/RepoCard/styles.js
+++ src/components/RepoCard/styles.js
@@ -10,24 +10,50 @@ export const RepoCardStyled = styled.div`
   flex-direction: column;
   gap: 10px;
   align-items: start;
+`;
 
-  h2 {
-    margin-top: 0;
-    font-size: 1.2rem;
-    font-weight: 500;
-    color: #333;
-  }
+export const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+`;
 
-  p {
-    font-size: 0.9rem;
-    color: #666;
-    flex-grow: 1;
-  }
+export const RepoName = styled.h2`
+  margin-top: 0;
+  font-size: 1.2rem;
+  font-weight: 500;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
+`;
 
-  a {
-    color: #0077b6;
-    text-decoration: none;
-    align-self: flex-end;
-    font-weight: 500;
+export const RepoDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 0.9rem;
+  margin-bottom: 8px;
+`;
+
+export const Description = styled.p`
+  flex-grow: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+export const GitHubLink = styled.a`
+  color: #0077b6;
+  text-decoration: none;
+  align-self: flex-start;
+  font-weight: 500;
+  margin-top: 10px;
+
+  &:hover {
+    text-decoration: underline;
   }
 `;
